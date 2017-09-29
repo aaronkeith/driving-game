7a47568a95fe0b17ee19196ee778a814aa652334
aaronkeith <aflisik@gmail.com>
2017-09-29 13:14:29 -0700

    User can start the car #3

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 window.onload = function()
 {
-const canvas = document.getElementById("canvas")
-const context = canvas.getContext("2d")
+x = 0;
+y = 0;
 
-const x = 0
-const y = 0
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
 
+var moveInterval = setInterval(function()
+{
+draw()
+}, 30)
+}
+
+function draw()
+{
+context.clearRect(0, 0, 1300, 1300)
 const car = new Image()
 car.src="images/kimi_2.png"
 car.addEventListener('load', function() {
 context.drawImage(car, x, y)
 }, false);
 
+y++;
 }
